b9d88d9e9739a21e6ac01d3b9d20f93f2957303a
flatbox <jorge@flatbox.co>
2017-08-04 12:08:20 -0600

    Updated Software
    se cambio la logica.... y otro monton de cosas mas...

--- Aguainfo_Teensy3_5_Disp_TFT_ST3575.ino
+++ Aguainfo_Teensy3_5_Disp_TFT_ST3575.ino
@@ -1,28 +1,25 @@
 /***************************************************
-  This is an example sketch for the Adafruit 1.8" SPI display.
-
-This library works with the Adafruit 1.8" TFT Breakout w/SD card
-  ----> http://www.adafruit.com/products/358
-The 1.8" TFT shield
-  ----> https://www.adafruit.com/product/802
-The 1.44" TFT breakout
-  ----> https://www.adafruit.com/product/2088
-as well as Adafruit raw 1.8" TFT display
-  ----> http://www.adafruit.com/products/618
-
-  Check out the links above for our tutorials and wiring diagrams
-  These displays use SPI to communicate, 4 or 5 pins are required to
-  interface (RST is optional)
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.
+  This is the first version for an automatic waterDispenser Logger
+  Hardware:
+  Microcontroller: 
+    Teensy 3.5 https://www.pjrc.com/store/teensy35.html
+  Sensors:
+    Reed Water Flow sensor 1/8" http://a.co/1OLH5OE
+    Time Of FLigth VL53L0X distance Sensor https://www.pololu.com/product/2490
+  Actuators:
+    1.8TFT 128x160 Color Screen   https://www.aliexpress.com/item/Consumer-Electronics-Shop-Free-shipping-1-8-Serial-128X160-SPI-TFT-LCD-Module-Display-PCB-Adapter/32602828489.html
+    10A 250VAC SRD-05VDC-SL-C Relay 
+    12V RGB LED STRIP  
+    12V Normally Closed Solenoid Valve http://a.co/acy6ti3
+  PowerSupply:
+    110-220VAC Dual output 5V 1A - 12V 1A power suppply http://a.co/2KBeenJ
+        
+  Written by Edwin Kestler For Aguainfo Guatemala.
   MIT license, all text above must be included in any redistribution
  ****************************************************/
 
 // This Teensy3 native optimized version requires specific pins
-//
+
 #define TFT_SCLK 13  // SCLK can also use pin 14
 #define TFT_MOSI 11  // MOSI can also use pin 7
 #define TFT_CS   10  // CS & DC can use pins 2, 6, 9, 10, 15, 20, 21, 22, 23
@@ -36,14 +33,35 @@ as well as Adafruit raw 1.8" TFT display
 #include "valve.h"
 #include <Fonts/FreeSerifBold18pt7b.h>
 #include <Fonts/FreeSerif9pt7b.h>
-
 #include <Wire.h>
 #include <VL53L0X.h>
 
 VL53L0X sensor;
+
 int distance, OldDistance;
 boolean Bottle, OldBottle;
 volatile float waterFlow;
+volatile float oldwaterflow;
+//-------------------------------------------------From strings Demo--------------------------------------------
+String text ="";
+int TextColor = 0;
+const int NUMBER_OF_ELEMENTS = 11;
+const int MAX_SIZE = 70;
+
+char descriptions [NUMBER_OF_ELEMENTS] [MAX_SIZE] = { 
+ { " ¡Luchemos por un país lleno de agua limpia! " }, 
+ { " Una botella plástica menos en ríos y mares, ¡Gracias! " }, 
+ { " ¡Protejamos las fuentes de agua para las futuras generaciones! " }, 
+ { " La magia de este planeta está en el agua " }, 
+ { " El agua es vida, no la desperdiciemos " }, 
+ { " ¡Beber agua te reanima y te regala sonrisas! " }, 
+ { " Proteger el agua es proteger a la humanidad " }, 
+ { " Cuidar el agua es cuidar tu salud " }, 
+ { " Tomar agua nos da vida. Tomar conciencia nos dará agua " }, 
+ { " Beber agua es bueno para una piel más sana " },
+ { " Tú eres lo que bebes " }, 
+ };
+
 
 // Option 1: use any pins but a little slower
 ST7735_t3 tft = ST7735_t3(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
@@ -58,13 +76,15 @@ ST7735_t3 tft = ST7735_t3(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
 #define HIGH_SPEED
 //#define HIGH_ACCURACY
 
-unsigned long lastSenseMillis, StartMillis;
+unsigned long lastSenseMillis, lastFSenseMillis;
 unsigned long UPDATELASERM = 500UL; //Variable que define el tiempo a trancurrir despues de inicializado el reloj para enviar el primer mensake de MQTT en microsegundos (10*1000UL)= 10segundos
 
 Valve portA(6);
-int BotellaServidas, LitrosAhorrados;
+int BotellaServidas;
+int S = 0;
 
 void setup(void) {
+  
   pinMode(SD_CS, INPUT_PULLUP);  // don't touch the SD card
   Serial.begin(115200);
   Serial.print("hello!");
@@ -113,16 +133,25 @@ void setup(void) {
 }
 
 void pulse(){   //measure the quantity of square wave
-  waterFlow += 1.0 / 5880.0;
+  waterFlow += 1.0/1350;
+  //DisplayStrings();  
 }
+
 
 void loop() {
   if(millis() - lastSenseMillis > UPDATELASERM) {
     SenseDistance();
     lastSenseMillis = millis(); //Actulizar la ultima hora de envio
   }
-  
-  if (distance != OldDistance){
+   
+  CheckBottle();
+  CheckWaterFlow();
+  IOValve();
+     
+}
+
+void CheckBottle(){
+   if (distance != OldDistance){
     if (distance < 100){
       Bottle = 1;
     }
@@ -130,50 +159,104 @@ void loop() {
       Bottle = 0;
     }
     OldDistance = distance;
+  }  
+}
+
+void CheckWaterFlow(){
+  if(millis() - lastFSenseMillis > UPDATELASERM) {
+    Serial.print(F("WaterinLtrs: "));
+    Serial.println(waterFlow);
+    Serial.print(F("difference: "));
+    Serial.println(waterFlow - oldwaterflow);
+    lastFSenseMillis = millis(); //Actulizar la ultima hora de envio      
   }
   
-  ScreenUpdate();
+  if (waterFlow - oldwaterflow >= 0.1){
+    DisplayStrings();    
+    oldwaterflow = waterFlow;
+    ScreenUpdate();
+  }
+}
+
+void IOValve(){
+  if (Bottle != OldBottle){
+    if (Bottle !=0){
+      portA.Update(LOW);
+      }else {
+        portA.Update(HIGH);
+        ScreenUpdate();
+        BotellaServidas ++;
+      }
+   }
+   OldBottle = Bottle;  
+}
+
+
+/*void FillBottle(){
+  if (oldwaterflow - waterFlow < 1 ){
+    portA.Update(LOW);
+    LitrosAhorrados ++;
+    oldwaterflow = waterFlow ;
+    }else{
+      portA.Update(HIGH);
+      BotellaServidas ++;
+    }
+    OldBottle = Bottle;
+}        */
+ 
+void DisplayStrings(){ 
+  tft.fillScreen(ST7735_BLACK);
+  if (TextColor == 0){
+    tft.setTextColor(ST7735_BLUE, ST7735_BLACK);  // White on black
+    TextColor = 1;
+  }else{
+    tft.setTextColor(ST7735_GREEN, ST7735_BLACK);  // White on black
+    TextColor = 0;
+  }
+  tft.setTextWrap(false);  // Don't wrap text to next line
+  tft.setFont();
+  tft.setTextSize(5);  // large letters
+  tft.setRotation(1); // horizontal display
+  text = descriptions [S];
+  //Serial.println (descriptions [i]);
+  const int width = 5;  // width of the marquee display (in characters)
+  // Loop once through the string
+  for (unsigned int offset = 0; offset < text.length(); offset++){
+    // Construct the string to display for this iteration
+    String t = "";
+    for (int i = 0; i < width; i++)
+    t += text.charAt((offset + i) % text.length()-1);
+    // Print  the string for this iteration
+    tft.setCursor(0, tft.height()/2-10);  // display will be halfway down screen
+    tft.print(t);
+    // Short delay so the text doesn't move too fast
+    delay(200);
+  }
+  if(S < NUMBER_OF_ELEMENTS){
+    S++;
+  }else{
+    S=0;
+  }
 }
 
 void ScreenUpdate(){
-  if (Bottle != OldBottle){
-    if (Bottle == 1){
-      portA.Update();
-      LitrosAhorrados ++;
-      tft.fillScreen(ST7735_BLACK);
-      tft.setCursor(50, 10);
-      tft.setTextColor(ST7735_GREEN);
-      tft.setTextSize(2);
-      Serial.print(F("LLenando"));
-      //tft.println("LLENANDO");
-    }
-    else{
-      portA.Update();
-      BotellaServidas ++;
-      tft.fillScreen(ST7735_BLACK);
-      tft.setCursor(30, 10);
-      tft.setTextColor(ST7735_GREEN);
-      tft.setTextSize(2);
-      Serial.print(F("ESPERANDO"));
-      //tft.println("ESPERANDO");
-      tft.setFont(&FreeSerifBold18pt7b);
-      tft.setCursor(50, 50);
-      tft.setTextSize(1);
-      tft.println(BotellaServidas);
-      tft.setCursor(50, 100);
-       tft.setTextColor(ST7735_BLUE);
-      tft.setTextSize(1);
-      tft.print(waterFlow,1);
-      tft.println(F("L"));
-      
-    }
-    OldBottle = Bottle;
-  }
+  tft.fillScreen(ST7735_BLACK);
+  tft.setCursor(30, 10);
+  tft.setTextColor(ST7735_GREEN);
+  tft.setFont(&FreeSerifBold18pt7b);
+  tft.setCursor(50, 50);
+  tft.setTextSize(1);
+  tft.println(BotellaServidas);
+  tft.setCursor(50, 100);
+  tft.setTextColor(ST7735_BLUE);
+  tft.setTextSize(1);
+  tft.print(waterFlow,1);
+  tft.println(F("L"));
 }
 
 void SenseDistance(){
   distance = sensor.readRangeSingleMillimeters();
-  Serial.print(distance);
+  //Serial.print(distance);
   if (sensor.timeoutOccurred()) {
     tft.fillScreen(ST7735_BLACK);
     tft.setCursor(10, 35);
--- valve.h
+++ valve.h
@@ -1,34 +1,25 @@
-class Valve
-{
+class Valve{
   // Class Member Variables
   // These are initialized at startup
   int ValvePin;      // the number of the Valve pin
- 
   // These maintain the current state
   int ValveState;                 // ledState used to set the LED
-
- 
   // Constructor - creates a Flasher 
   // and initializes the member variables and state
   public:
-  Valve(int pin)
-  {
-  ValvePin = pin;
-  pinMode(ValvePin, OUTPUT);     
-  ValveState = LOW; 
+  Valve(int pin){
+    ValvePin = pin;
+    pinMode(ValvePin, OUTPUT);
   }
- 
-  void Update()
-  {
-    if(ValveState == HIGH)
-    {
+  
+  void Update(int State){
+    ValveState = State;
+    if(ValveState == HIGH){
       ValveState = LOW;  // Turn it off
       digitalWrite(ValvePin, ValveState);  // Update the actual Valve
       Serial.print(F("OFF"));
       Serial.print(F(""));
-    }
-    else if (ValveState == LOW)
-    {
+    } else if (ValveState == LOW){
       ValveState = HIGH;  // turn it on
       digitalWrite(ValvePin, ValveState);   // Update the actual Valve
       Serial.print(F("ON"));
